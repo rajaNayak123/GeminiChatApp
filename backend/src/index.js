f2bf8badf7bab155504db68b0da2bc0d2d0fc05c
rajaNayak123 <nayakraja151@gmail.com>
2025-02-26 23:55:02 +0530

    Complete socketio setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,33 @@ import { app } from "./app.js";
 import dotenv from "dotenv"
 dotenv.config({path:"./.env"})
 
+import {Server} from 'socket.io';
+import http from 'http'
+
+
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors: {
+        origin: "*", // Change in production
+        methods: ["GET", "POST"]
+    },
+    path: "/socket.io/" // Ensure correct path
+});
+
+io.on("connection", (socket) => {
+    console.log("🟢 Socket.IO: Connection established");
+
+    socket.on("event", (data) => {
+        console.log("📩 Received event:", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("🔴 User disconnected");
+    });
+});
+
 connectToDb().then(()=>{
-    app.listen(process.env.PORT||8000, ()=>{
+    server.listen(process.env.PORT||8000, ()=>{
         console.log(`Server listening on port ${process.env.PORT}`);
     })
 }).catch((error)=>{
